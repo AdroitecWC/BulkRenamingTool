7a2dc88905f0faacd089c0c607cd2c245af8bfb2
pavan-koda <pavankumar.k@adroitecengg.com>
2025-07-14 14:25:53 +0530

    some updates

---
+++ RenaimingToolCS/CreoFunctions/OpenAndCloseCreo.cs
@@ -0,0 +1,267 @@
+﻿using pfcls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RenaimingToolCS.CreoFunctions
+{
+
+    internal class OpenAndCloseCreo
+    {
+        #region Variables
+        private string _strWorkingDirectory;
+        private string _strProEExePath;
+        private bool _blnIsProERunning;
+        private Exception _oErrorObject;
+        private string _strErrorMessage;
+        private IpfcAsyncConnection _iIpfcAsynConnection;
+        private CCpfcAsyncConnection _iCcpfcAsynConnection;
+        private IpfcBaseSession _session;
+        #endregion
+
+        #region Properties
+        public string WorkingDirectory
+        {
+            get { return _strWorkingDirectory; }
+        }
+
+        public string ProEExePath
+        {
+            get { return _strProEExePath; }
+        }
+
+        public bool IsProERunning
+        {
+            get { return _blnIsProERunning; }
+            set { _blnIsProERunning = value; }
+        }
+
+        public Exception ErrorObject
+        {
+            get { return _oErrorObject; }
+        }
+
+        public string ErrorMessage
+        {
+            get { return _strErrorMessage; }
+        }
+
+        public IpfcAsyncConnection IpfcAsynchronousConnection
+        {
+            get { return _iIpfcAsynConnection; }
+            set { _iIpfcAsynConnection = value; }
+        }
+
+        public CCpfcAsyncConnection CcpfcAsynchronousConnection
+        {
+            get { return _iCcpfcAsynConnection; }
+            set { _iCcpfcAsynConnection = value; }
+        }
+
+        public IpfcBaseSession Session
+        {
+            get { return _session; }
+            set { _session = value; }
+        }
+        #endregion
+
+        public bool RunProe(string strWorkingDirectory)
+        {
+            bool result = true;
+            try
+            {
+                if (!CheckProEOpened(strWorkingDirectory))
+                {
+                    foreach (var proc in System.Diagnostics.Process.GetProcessesByName("pfclscom"))
+                    {
+                        if (!proc.HasExited)
+                        {
+                            proc.Kill();
+                        }
+                    }
+
+                    string proePath = Environment.GetEnvironmentVariable("PROE_PATH");
+                    string myDocsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    string startPath = System.IO.Path.Combine(myDocsPath, "\\");
+                    System.Diagnostics.Process.Start(proePath, startPath);
+
+                    System.Threading.Thread.Sleep(5000);
+
+                    _iCcpfcAsynConnection = new CCpfcAsyncConnection();
+                    _iIpfcAsynConnection = _iCcpfcAsynConnection.Connect(DBNull.Value, DBNull.Value, DBNull.Value, DBNull.Value);
+                    Session = (IpfcBaseSession)_iIpfcAsynConnection.Session;
+                }
+
+                // Set Working Directory
+                if (_iIpfcAsynConnection.IsRunning())
+                {
+                    _strWorkingDirectory = strWorkingDirectory;
+                    Session.ChangeDirectory(strWorkingDirectory);
+                }
+            }
+            catch (Exception oException)
+            {
+                result = false;
+                _oErrorObject = oException;
+                _strErrorMessage = "Unable to start process of ProE application" + Environment.NewLine +
+                                   "Working directory given " + strWorkingDirectory + Environment.NewLine + Environment.NewLine +
+                                   "System generated error:-" + Environment.NewLine + oException.Message;
+                MessageBox.Show(_strErrorMessage);
+            }
+
+            return result;
+        }
+        private bool CheckProEOpened(string strWorkingDirectory)
+        {
+            string errMsg = "";
+
+            if (!(System.Diagnostics.Process.GetProcessesByName("nmsd").Length > 0))
+            {
+                errMsg = "Name service is not running on the system.";
+            }
+
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PRO_COMM_MSG_EXE")))
+            {
+                if (string.IsNullOrEmpty(errMsg))
+                {
+                    errMsg = "Environment variable 'PRO_COMM_MSG_EXE' has not been set.";
+                }
+                else
+                {
+                    errMsg += Environment.NewLine + "Environment variable 'PRO_COMM_MSG_EXE' has not been set.";
+                }
+            }
+
+            if (Environment.GetEnvironmentVariable("PRO_DIRECTORY") == null)
+            {
+                if (string.IsNullOrEmpty(errMsg))
+                {
+                    errMsg = "Environment variable 'PRO_DIRECTORY' has not been set.";
+                }
+                else
+                {
+                    errMsg += Environment.NewLine + "Environment variable 'PRO_DIRECTORY' has not been set.";
+                }
+            }
+
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROE_PATH")))
+            {
+                if (string.IsNullOrEmpty(errMsg))
+                {
+                    errMsg = "Environment variable 'PROE_PATH' has not been set.";
+                }
+                else
+                {
+                    errMsg += Environment.NewLine + "Environment variable 'PROE_PATH' has not been set.";
+                }
+            }
+
+            bool result = true;
+            try
+            {
+                _iCcpfcAsynConnection = new CCpfcAsyncConnection();
+
+                if (_iIpfcAsynConnection == null)
+                {
+                    _iIpfcAsynConnection = CcpfcAsynchronousConnection.Start(
+                        Environment.GetEnvironmentVariable("PROE_PATH"),
+                        strWorkingDirectory);
+                }
+
+                // ✅ Assign the session properly before using
+                Session = (IpfcBaseSession)_iIpfcAsynConnection.Session;
+
+                if (_iIpfcAsynConnection.IsRunning())
+                {
+                    _strWorkingDirectory = strWorkingDirectory;
+                    Session.ChangeDirectory(strWorkingDirectory);
+                }
+            
+
+
+
+                // Set working directory
+                _strWorkingDirectory = strWorkingDirectory;
+                _blnIsProERunning = true;
+
+                System.Threading.Thread.Sleep(1000);
+            }
+            catch (Exception oException)
+            {
+                CloseProe();
+                KillCreO();
+                _oErrorObject = oException;
+                result = false;
+                _strErrorMessage = "Unable to Connect Active ProE" + Environment.NewLine +
+                                   "Application is Trying to Open ProE" + Environment.NewLine +
+                                   "System generated error:-" + Environment.NewLine + oException.Message;
+                MessageBox.Show(_strErrorMessage);
+            }
+
+            return result;
+        }
+
+        public bool CloseProe()
+        {
+            bool result = true;
+            try
+            {
+                // Check for the object IpfcAsynconnection and if it is running
+                if (_iIpfcAsynConnection != null)
+                {
+                    if (_iIpfcAsynConnection.IsRunning())
+                    {
+                        _iIpfcAsynConnection.End();
+                    }
+
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(_iIpfcAsynConnection);
+                    _iIpfcAsynConnection = null;
+                    GC.Collect();
+                }
+            }
+            catch (Exception oException)
+            {
+                result = false;
+                _oErrorObject = oException;
+                _strErrorMessage = "Unable to Close ProE" + Environment.NewLine +
+                                   "System generated error:-" + Environment.NewLine + oException.Message;
+                MessageBox.Show(_strErrorMessage);
+            }
+            return result;
+        }
+
+        public void KillCreO()
+        {
+            try
+            {
+                foreach (var proc in System.Diagnostics.Process.GetProcessesByName("xtop"))
+                {
+                    if (!proc.HasExited)
+                    {
+                        if (!proc.Responding)
+                        {
+                            proc.Kill();
+                        }
+                        proc.Kill();
+                    }
+                }
+
+                foreach (var proc in System.Diagnostics.Process.GetProcessesByName("nmsd"))
+                {
+                    if (!proc.HasExited)
+                    {
+                        proc.Kill();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Suppress any exceptions
+            }
+        }
+
+    }
+}
---
+++ RenaimingToolCS/ViewModels/RenamingViewModel.cs
@@ -0,0 +1,431 @@
+﻿using ClosedXML.Excel;
+using Ookii.Dialogs.Wpf;
+using pfcls;
+
+using RenaimingToolCS.CreoFunctions;
+using RenaimingToolCS.Helpers;
+using RenaimingToolCS.ViewModels.Creo;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace RenaimingToolCS.ViewModels
+{
+    /// <summary>
+    /// ViewModel class that handles logic for the renaming tool.
+    /// Binds to RenamingView.xaml to handle user input and display files.
+    /// </summary>
+    internal class RenamingViewModel : ObservableObject
+    {
+        private string _inputFolderPath;
+        private string _outputFolderPath;
+        private string _excelFilePath;
+
+        public ICommand DownloadExcelCommand { get; }
+        public ICommand RenameFilesCommand { get; }
+
+        public ICommand BrowseInputFolderCommand { get; }
+        public ICommand BrowseOutputFolderCommand { get; }
+        public ICommand BrowseExcelFileCommand { get; }
+
+
+
+        public RenamingViewModel()
+        {
+            DownloadExcelCommand = new RelayCommand(DownloadExcel);
+            RenameFilesCommand= new RelayCommand(RenameCreoFiles);
+            BrowseInputFolderCommand = new RelayCommand(BrowseInputFolder);
+            BrowseOutputFolderCommand = new RelayCommand(BrowseOutputFolder);
+            BrowseExcelFileCommand = new RelayCommand(BrowseExcelFile);
+
+        }
+        private void BrowseInputFolder()
+        {
+            var dialog = new VistaFolderBrowserDialog
+            {
+                Description = "Select Input Folder",
+                UseDescriptionForTitle = true,
+                ShowNewFolderButton = true
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                InputFolderPath = dialog.SelectedPath;
+                LoadFilesFromInputFolder(InputFolderPath);
+            }
+        }
+
+
+
+
+        private void BrowseOutputFolder()
+        {
+            var dialog = new VistaFolderBrowserDialog
+            {
+                Description = "Select Output Folder",
+                UseDescriptionForTitle = true,
+                ShowNewFolderButton = true
+            }
+            ;
+
+            if (dialog.ShowDialog() == true)
+            {
+                OutputFolderPath = dialog.SelectedPath;
+            }
+        }
+
+
+        private void BrowseExcelFile()
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Excel Files (*.xlsx;*.xls)|*.xlsx;*.xls"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                ExcelFilePath = dialog.FileName;
+                LoadExcelMapping(ExcelFilePath);
+            }
+        }
+
+        /// <summary>
+        /// Path of the folder containing files to be renamed.
+        /// </summary>
+        public string InputFolderPath
+        {
+            get => _inputFolderPath;
+            set => SetProperty(ref _inputFolderPath, value);
+        }
+
+        /// <summary>
+        /// Path of the folder where renamed files will be stored.
+        /// </summary>
+        public string OutputFolderPath
+        {
+            get => _outputFolderPath;
+            set => SetProperty(ref _outputFolderPath, value);
+        }
+        public string ExcelFilePath  // <--- add this property
+        {
+            get => _excelFilePath;
+            set => SetProperty(ref _excelFilePath, value);
+        }
+
+        /// <summary>
+        /// List of files displayed in the DataGrid with their original and new names.
+        /// </summary>
+        public ObservableCollection<FileModel> Files { get; } = new ObservableCollection<FileModel>();
+
+        /// <summary>
+        /// Loads file names from the input folder, applies Creo purge, and fills the Files collection.
+        /// </summary>
+        /// <param name="folderPath">Path to the input folder</param>
+        public void LoadFilesFromInputFolder(string folderPath)
+        {
+            Files.Clear();
+            if (!Directory.Exists(folderPath))
+                return;
+
+            CreoFileHelper.PurgeFolder(folderPath);
+
+            Files.Clear();
+
+            var files = Directory.GetFiles(folderPath);
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+
+                Files.Add(new FileModel
+                {
+                    OriginalName = fileName,
+                    OldName = string.Empty,  // Initially empty or same as OriginalName
+                    NewName = string.Empty
+                });
+            }
+        }
+
+        private string NormalizeCreoName(string fileName)
+        {
+            // Remove known Creo extensions with version numbers
+            var pattern = @"\.(prt|asm|drw|frm|sec)\.\d+$";
+            return Regex.Replace(fileName, pattern, "", RegexOptions.IgnoreCase);
+        }
+
+        public void LoadExcelMapping(string excelPath)
+        {
+            if (!File.Exists(excelPath))
+            {
+                MessageBox.Show("Excel file not found.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var mapping = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            var oldNameSet = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            var newNameSet = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            var duplicateLog = new StringBuilder();
+
+            try
+            {
+                using (var workbook = new XLWorkbook(excelPath))
+                {
+                    var worksheet = workbook.Worksheet(1);
+
+                    foreach (var row in worksheet.RowsUsed().Skip(1))
+                    {
+                        string rawOld = row.Cell(1).GetString().Trim();
+                        string newName = row.Cell(2).GetString().Trim();
+
+                        if (string.IsNullOrWhiteSpace(rawOld) || string.IsNullOrWhiteSpace(newName))
+                            continue;
+
+                        // Consistently normalize
+                        string normalizedOld = NormalizeCreoName(rawOld).Trim().ToLowerInvariant();
+                        string normalizedNew = newName.Trim();
+
+                        if (!oldNameSet.Add(normalizedOld))
+                        {
+                            duplicateLog.AppendLine($"Duplicate OLD name skipped: '{normalizedOld}'");
+                            continue;
+                        }
+
+                        if (!newNameSet.Add(normalizedNew))
+                        {
+                            duplicateLog.AppendLine($"Duplicate NEW name skipped: '{normalizedNew}'");
+                            continue;
+                        }
+
+                        mapping[normalizedOld] = normalizedNew;
+                    }
+                }
+                var uniqueFiles = Files
+                     .GroupBy(f => NormalizeCreoName(f.OriginalName).Trim().ToLowerInvariant())
+                     .Select(g => g.First()) // Only one file per normalized name
+                     .ToList();
+
+                // Then map only these:
+                foreach (var file in uniqueFiles)
+                {
+                    string fileKey = NormalizeCreoName(file.OriginalName).Trim().ToLowerInvariant();
+                    file.OldName = fileKey;
+
+                    if (mapping.TryGetValue(fileKey, out string mappedNewName))
+                    {
+                        file.NewName = mappedNewName;
+                    }
+                }
+
+                if (duplicateLog.Length > 0)
+                {
+                    MessageBox.Show(
+                        $"Duplicate entries found and skipped:\n\n{duplicateLog}",
+                        "Duplicate Warning",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to load Excel mapping: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+
+        private string StripCreoExtensions(string fileName)
+        {
+            // Handles files like "PART1.prt.1" or "ASSY1.asm.9"
+            string[] knownExtensions = { ".prt", ".asm", ".drw" };
+
+            foreach (var ext in knownExtensions)
+            {
+                int extIndex = fileName.IndexOf(ext + ".");
+                if (extIndex >= 0)
+                {
+                    return fileName.Substring(0, extIndex);
+                }
+            }
+
+            // If no match found, fallback to removing after last dot (optional)
+            int lastDotIndex = fileName.LastIndexOf('.');
+            if (lastDotIndex > 0)
+            {
+                return fileName.Substring(0, lastDotIndex);
+            }
+
+            return fileName;
+        }
+
+
+        private void DownloadExcel()
+        {
+            try
+            {
+                var saveFileDialog = new Microsoft.Win32.SaveFileDialog
+                {
+                    Filter = "Excel Workbook (*.xlsx)|*.xlsx",
+                    FileName = "FileList.xlsx"
+                };
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    using (var workbook = new ClosedXML.Excel.XLWorkbook())
+                    {
+                        var worksheet = workbook.Worksheets.Add("Files");
+
+                        worksheet.Cell(1, 1).Value = "File Path";
+                        worksheet.Cell(1, 2).Value = "Original Name";
+
+                        int row = 2;
+                        foreach (var file in Files)
+                        {
+                            worksheet.Cell(row, 1).Value = file.FullPath;       // Add FullPath property in your model
+                            worksheet.Cell(row, 2).Value = file.OriginalName;
+                            row++;
+                        }
+
+                        workbook.SaveAs(saveFileDialog.FileName);
+                    }
+
+                    MessageBox.Show("Excel file exported successfully.", "Export", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to export Excel: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private void RenameCreoFiles()
+        {
+            OpenAndCloseCreo creo = new OpenAndCloseCreo();
+            var renamedLog = new StringBuilder();
+            var errorLog = new StringBuilder();
+            // Initialize Creo session
+            
+
+            try
+            {
+                var WorkingDir = InputFolderPath;
+                creo.RunProe(WorkingDir);
+                CreoSessionManager.Instance.InitializeCreoSession();
+                IpfcBaseSession session = CreoSessionManager.Instance.Session;
+                session.EraseUndisplayedModels();
+                // 1. Purge and open all drawings in folder
+                CreoFileHelper.PurgeFolder(InputFolderPath);
+                CreoFileHelper.OpenAllDrawingsInFolder(InputFolderPath);
+
+
+
+                var baseSession = (IpfcBaseSession)session;
+                var loadedModels = baseSession.ListModels();
+
+                // 2. Create rename map from Files collection (DataGrid)
+                var renameMap = Files
+                    .Where(f => !string.IsNullOrWhiteSpace(f.OldName) && !string.IsNullOrWhiteSpace(f.NewName))
+                    .ToDictionary(f => f.OldName, f => f.NewName);
+
+                // 3. Rename logic
+                foreach (IpfcModel model in loadedModels)
+                {
+                    var currentName = model.InstanceName;
+
+                    // Only match model base name (remove extensions)
+                    var baseName = StripCreoExtensions(currentName);
+
+                    if (renameMap.TryGetValue(baseName, out var newName))
+                    {
+                        if (!string.Equals(baseName, newName, StringComparison.OrdinalIgnoreCase))
+                        {
+                            try
+                            {
+                                model.Rename(newName, true);
+                                renamedLog.AppendLine($"Renamed '{baseName}' to '{newName}'");
+                            }
+                            catch (Exception ex)
+                            {
+                                errorLog.AppendLine($"Failed to rename '{baseName}' to '{newName}': {ex.Message}");
+                            }
+                        }
+                    }
+                }
+
+                // 4. Log results
+                var finalLog = new StringBuilder();
+                if (renamedLog.Length > 0)
+                {
+                    finalLog.AppendLine("Renamed Models:\n" + renamedLog.ToString());
+                }
+
+                if (errorLog.Length > 0)
+                {
+                    finalLog.AppendLine("Errors:\n" + errorLog.ToString());
+                }
+
+                MessageBox.Show(finalLog.ToString(), "Rename Result", MessageBoxButton.OK, MessageBoxImage.Information);
+                creo.KillCreO();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unexpected error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                creo.KillCreO();
+            }
+        }
+        private void MoveRenamedFilesToOutputFolder(string baseName, string sourceFolder, string destinationFolder)
+        {
+            // Move all related Creo files (.prt.*, .asm.*, .drw.*) with the base name
+            string[] extensions = { ".prt", ".asm", ".drw" };
+
+            foreach (var ext in extensions)
+            {
+                var files = Directory.GetFiles(sourceFolder, $"{baseName}{ext}.*"); // handles .prt.1, .asm.2 etc.
+
+                foreach (var file in files)
+                {
+                    var fileName = Path.GetFileName(file);
+                    var destPath = Path.Combine(destinationFolder, fileName);
+
+                    try
+                    {
+                        File.Copy(file, destPath, overwrite: true);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Failed to move file {fileName}: {ex.Message}", "Move Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+            }
+        }
+
+
+
+    }
+
+    /// <summary>
+    /// Represents a single file with original and new name properties.
+    /// </summary>
+    internal class FileModel : ObservableObject
+    {
+        public string OriginalName { get; set; }
+        private string _oldName;
+        public string OldName
+        {
+            get => _oldName;
+            set => SetProperty(ref _oldName, value);
+        }
+
+        private string _newName;
+        public string NewName
+        {
+            get => _newName;
+            set => SetProperty(ref _newName, value);
+        }
+        public string FullPath { get; set; }
+    }
+}
